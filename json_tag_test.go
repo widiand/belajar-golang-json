fd32bfc1617e336fd3522698152479584b7a7e0a
widiand <widiand36@gmail.com>
2025-09-06 17:23:01 +0700

    json tag

---
+++ json_tag_test.go
@@ -0,0 +1,34 @@
+package belajargolangjson
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Product struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	ImageURL string `json:"image_url"`
+}
+
+func TestJSONTag(t *testing.T) {
+	product := Product{
+		Id:       "P0001",
+		Name:     "Apple Macbook pro",
+		ImageURL: "http://example.com/image.png",
+	}
+
+	bytes, _ := json.Marshal(product)
+	fmt.Println(string(bytes))
+}
+
+func TestJSONTagDecode(t *testing.T) {
+	jsonString := `{"id":"P0001","name":"Apple Macbook pro","image_url":"http://example.com/image.png"}`
+	jsonBytes := []byte(jsonString)
+
+	product := &Product{}
+
+	json.Unmarshal(jsonBytes, product)
+	fmt.Println(product)
+}
